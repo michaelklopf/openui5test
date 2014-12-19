743575e3cc005da4d17b5ee14b7bb1994cd739fc
Michael <klopf.michael@gmail.com>
2014-12-19 08:30:09 +0100

    Added documentation

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var concat = require('gulp-concat');
 
+// Take main.js and link all module calls, we set up with Browserify
 gulp.task('browserify', function() {
     gulp.src('static/js/main.js')
         .pipe(browserify())
@@ -9,6 +10,7 @@ gulp.task('browserify', function() {
         .pipe(gulp.dest('dist/js'));
 });
 
+// Copy css, js, and html files in dist folder
 gulp.task('copy', function() {
    gulp.src('views/index.html')
     .pipe(gulp.dest('dist'));
@@ -21,8 +23,11 @@ gulp.task('copy', function() {
 
 });
 
+// Create a task called default, which does the browserify step and then copies the files
 gulp.task('default', ['browserify', 'copy']);
 
+// Sets up a task to watch for changes in the source folder and start the default task automatically
+// Apparently works only with *unix shell
 gulp.task('watch', function() {
    gulp.watch('views/**/*.*', ['default'])
    	   .watch('static/**/*.*', ['default']); 
